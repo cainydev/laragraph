fd334c13757d3958251a6b878bfcf50ea5cacb49
cainydev <wajo432@gmail.com>
2026-03-21 13:05:11 +0100

    chore: rebrand skeleton to laragraph, add prism-php/prism dependency

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Cainy\Laragraph\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,5 @@
 <?php
 
-use VendorName\Skeleton\Tests\TestCase;
+use Cainy\Laragraph\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
